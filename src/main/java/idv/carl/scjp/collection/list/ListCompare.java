b5fa98ae24b97483576b463d1c58e8a7455a2f60
Carl <yotsuba1022@gmail.com>
2017-12-17 17:40:11 +0800

    Add simple example for HashSet v.s. TreeSet.

---
+++ src/main/java/idv/carl/scjp/collection/list/ListCompare.java
@@ -0,0 +1,19 @@
+package idv.carl.scjp.collection.list;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Carl Lu
+ */
+public class ListCompare {
+
+    private List<Integer> linkedList = new LinkedList<>();
+    private List<Integer> arrayList = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/idv/carl/scjp/domain/Student.java
@@ -0,0 +1,69 @@
+package idv.carl.scjp.domain;
+
+import java.util.Objects;
+
+/**
+ * @author Carl Lu
+ */
+public class Student implements Comparable {
+
+    private Long id;
+    private String name;
+    private Integer age;
+
+    public Student(Long id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id) && Objects.equals(name, student.name) && Objects.equals(age, student.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "id=" + id + ", name='" + name + '\'' + ", age=" + age + '}';
+    }
+
+    @Override
+    public int compareTo(Object another) {
+        return this.getId().compareTo(( (Student) another ).getId());
+    }
+
+}
